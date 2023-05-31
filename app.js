64372ec48654ff0545ab7cf904f557d22900792e
Peterson Moro <peterson.airesmoro@hotmail.com>
2023-05-31 20:16:31 -0300

    Aula 02

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ app.get('/',(req,res) => { //set das rotas
     res.send('<h1>Minha Lista de Tarefas</h1>')
 })
 
+//Devolvendo uma resposta em formato JSON
+    /* Na URL utilizar o /json para obter a resposta do servidor */
+app.get('/json', (req,res) => {
+    res.json({title: 'Tarefa X', done: true});
+})
+
 app.listen(3000, () => { //Escuta porta 3000
     console.log('Servidor foi Iniciado');
 })
